de96861b90fc26c1ce66e92f81ff4d3260888c70
Yeshe Sartori <sartori.yeshe@gmail.com>
2022-03-11 17:37:43 +0100

    updates and cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 const { series, parallel, src, dest, watch } = require('gulp'),
-      sass = require('gulp-sass')(require('sass')),
-      concat = require('gulp-concat'),
-      merge2 = require('merge2'),
-      sourcemaps = require('gulp-sourcemaps'),
-      autoprefixer = require('gulp-autoprefixer'),
-      cleanDest = require('gulp-clean-dest'),
-      eslint = require('gulp-eslint'),
-      babel = require('gulp-babel'),
-      uglify = require('gulp-uglify'),
-      svgmin = require('gulp-svgmin'),
-      through2 = require('through2'),
-      log = require('fancy-log'),
-      minimist = require('minimist')(process.argv.slice(2));
+  sass = require('gulp-sass')(require('sass')),
+  concat = require('gulp-concat'),
+  merge2 = require('merge2'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  cleanDest = require('gulp-clean-dest'),
+  eslint = require('gulp-eslint'),
+  babel = require('gulp-babel'),
+  uglify = require('gulp-uglify'),
+  svgmin = require('gulp-svgmin'),
+  through2 = require('through2'),
+  log = require('fancy-log'),
+  minimist = require('minimist')(process.argv.slice(2))
 
 const config = {
   project: 'app',
   srcPath: 'src/',
   publicPath: 'static/',
-  forProduction: minimist.prod
+  forProduction: minimist.prod,
 }
 
 let location = {
@@ -26,41 +26,36 @@ let location = {
     js: config.publicPath + 'js',
   },
   sources: {
-    jsVendors: [
-      'node_modules/smoothscroll-polyfill/dist/smoothscroll.js',
-      config.srcPath + 'js/vendor/*.js'
-    ],
+    jsVendors: ['node_modules/smoothscroll-polyfill/dist/smoothscroll.js', config.srcPath + 'js/vendor/*.js'],
     jsApp: config.srcPath + 'js/*.js',
-    css: [
-      config.srcPath + 'sass/app.s*ss'
-    ],
-    frameworks: [
-
-    ]
-  }
+    css: [config.srcPath + 'sass/app.s*ss'],
+    frameworks: [],
+  },
 }
 
 function css() {
-  return src(location.sources.css, {allowEmpty: true})
+  return src(location.sources.css, { allowEmpty: true })
     .pipe(config.forProduction ? through2.obj() : sourcemaps.init())
     .pipe(config.forProduction ? sass({ includePaths: location.sources.frameworks, outputStyle: 'compressed' }).on('error', handleError) : sass({ includePaths: location.sources.frameworks }).on('error', handleError))
     .pipe(autoprefixer())
-  .pipe(concat(config.project + '.css'))
-  .pipe(cleanDest(location.compiled.css))
-  .pipe(config.forProduction ? through2.obj() : sourcemaps.write('maps'))
-  .pipe(dest(location.compiled.css));
+    .pipe(concat(config.project + '.css'))
+    .pipe(cleanDest(location.compiled.css))
+    .pipe(config.forProduction ? through2.obj() : sourcemaps.write('maps'))
+    .pipe(dest(location.compiled.css))
 }
 
 function js() {
   return merge2(
-      src(location.sources.jsVendors, { allowEmpty: true }),
-      src(location.sources.jsApp, {allowEmpty: true})
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(babel({
-          "presets": [["@babel/preset-env"]]
-        }))
-    )
+    src(location.sources.jsVendors, { allowEmpty: true }),
+    src(location.sources.jsApp, { allowEmpty: true })
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(
+        babel({
+          presets: [['@babel/preset-env']],
+        })
+      )
+  )
     .pipe(concat(config.project + '.js'))
     .pipe(config.forProduction ? uglify() : through2.obj())
     .pipe(cleanDest(location.compiled.js))
@@ -70,25 +65,29 @@ function js() {
 function svg() {
   return src(config.srcPath + 'svg/**/*.svg')
     .pipe(cleanDest(config.publicPath + 'svg'))
-    .pipe(svgmin({
-      plugins: [{
-        removeViewBox: false,
-        convertStyleToAttrs: false
-      }]
-    }))
-    .pipe(dest(config.publicPath + 'svg'));
+    .pipe(
+      svgmin({
+        plugins: [
+          {
+            removeViewBox: false,
+            convertStyleToAttrs: false,
+          },
+        ],
+      })
+    )
+    .pipe(dest(config.publicPath + 'svg'))
 }
 
-exports.default = parallel(css, js, svg);
+exports.default = parallel(css, js, svg)
 
-exports.watch = series(parallel(css, js, svg), w);
+exports.watch = series(parallel(css, js, svg), w)
 
-exports.svgmin = svg;
+exports.svgmin = svg
 
 function w() {
-  watch(config.srcPath + 'sass/**/*.s*ss', css);
-  watch(config.srcPath + 'js/**/*.js', js);
-  watch(config.srcPath + 'svg/**/*.svg', svg);
+  watch(config.srcPath + 'sass/**/*.s*ss', css)
+  watch(config.srcPath + 'js/**/*.js', js)
+  watch(config.srcPath + 'svg/**/*.svg', svg)
 }
 
 function handleError(err) {
